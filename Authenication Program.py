754d278b41de98ceff11d082fad8a03096669442
vketch15013880 <113266056+vketch15013880@users.noreply.github.com>
2023-09-26 14:55:59 -0400

    Update Authenication Program.py

--- Authenication Program.py
+++ Authenication Program.py
@@ -1,11 +1,11 @@
 #Requirments:
 #Username and Password: (Create several username and password)
 #Place to login
-#Store the Username and Password(Hash, Encryption)
-#No 2 users have the same username (email or phone or finital lastname or first.lastname)
+#Store the Username and Password(Dictionary)
+#No 2 users have the same username
 #Password Complexity Requirments (maximum password(69 chars), 6 chars, special char($,_,-,.,),1 num,
 #1 lowercase, 1 uppercase
-#Check password entered (type the password and retype it) eyeball
+#Check the password entered (type the password and retype it)
 
 #a page that directs users to make username or login or exit the program
 
