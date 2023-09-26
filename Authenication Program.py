a229ac235c9a0784976db4f4cc1cdeb5a9d7e9fb
vketch15013880 <113266056+vketch15013880@users.noreply.github.com>
2023-09-26 14:53:03 -0400

    Authentication Program
    The beginning layout of the authentication program in CIS2619

---
+++ Authenication Program.py
@@ -0,0 +1,57 @@
+#Requirments:
+#Username and Password: (Create several username and password)
+#Place to login
+#Store the Username and Password(Hash, Encryption)
+#No 2 users have the same username (email or phone or finital lastname or first.lastname)
+#Password Complexity Requirments (maximum password(69 chars), 6 chars, special char($,_,-,.,),1 num,
+#1 lowercase, 1 uppercase
+#Check password entered (type the password and retype it) eyeball
+
+#a page that directs users to make username or login or exit the program
+
+
+
+
+
+
+
+#Making a username and password
+
+
+
+
+
+#password recheck
+
+
+
+
+
+#Login 
+
+
+
+
+
+
+#after recieving validation they are given an option of returning to main menu or quiting the program
+
+
+
+__main__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
