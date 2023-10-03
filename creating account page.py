60efeb9c94717c37fce8f6c4537679050cc4ef99
vketch15013880 <113266056+vketch15013880@users.noreply.github.com>
2023-10-03 15:20:58 -0400

    Add files via upload

---
+++ creating account page.py
@@ -0,0 +1,17 @@
+print("Account Creation")
+print("At any time enter menu to return to the menu page")
+username = input("Enter a username:  ")
+print("Your password must contain")
+print("1. 6 characters")
+print("2. 1 Upppercase")
+print("3. 1 Lowercase")
+print("4. 1 Special Character ($ _ - .)")
+print("5. 1 Digit")
+print("You can enter menu at any time to return to the main menu")
+password = input("Enter a password")
+reenter = input("Reenter Password")
+    if password = reenter
+        print("You created an account")
+        print("Do you want to return to the main menu (y/n)")
+
+
