bc9980f0724b32e04aa59649d681e40cb8386e24
vketch15013880 <113266056+vketch15013880@users.noreply.github.com>
2023-10-03 14:59:16 -0400

    Update Authenication Program.py

--- Authenication Program.py
+++ Authenication Program.py
@@ -15,8 +15,28 @@
 
 #Making a username and password
 
+class Creatingaccountpage:
+  def store():
+    logins={}
 
+    username= input("Create a username:")
+    password=input("Create a password:")
 
+    logins[username] = password    
+    for login in logins:
+        print (logins)
+
+  def store():
+    store()
+    
+    
+  if __name__ == "__main__":
+    
+  main()print("Account Creation")
+  print("At any time enter menu to return to the menu page")
+  username = input("Enter a username:  ")
+  for username in set(store.values()):
+  
 
 
 #password recheck
